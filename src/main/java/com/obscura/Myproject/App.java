2bfee5f980cfda1c402d3947b66924583a52e776
aparnarajesh <aparnarajesh@Aparna>
2023-03-20 08:06:34 +0530

    i commited

---
+++ src/main/java/com/obscura/Myproject/App.java
@@ -0,0 +1,13 @@
+package com.obscura.Myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
